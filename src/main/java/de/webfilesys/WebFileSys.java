2dd592738f41be99bff087014fcb4dc61d409032
Leonardo Cardoso <leonardo.f.cardoso@gmail.com>
2016-07-10 22:44:57 -0300

    removed popup window from zip file download

--- src/main/java/de/webfilesys/WebFileSys.java
+++ src/main/java/de/webfilesys/WebFileSys.java
@@ -42,7 +42,7 @@ import de.webfilesys.watch.FolderWatchManager;
 public class WebFileSys {
 	private static WebFileSys instance = null;
 
-	public static final String VERSION = "Version 2.9.4-L (2016/07)";
+	public static final String VERSION = "Version 2.9.5-L (2016/07)";
 
 	public static final String THUMB_DIR = "thumbnails";
 
